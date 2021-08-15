796e02ec48a0715c14594d4bed9d9157fbe0b23f
Nenad020 <nenadzelenovic@gmail.com>
2021-08-15 16:49:26 +0200

    Ubacio izgled za import csv fajlova

---
+++ PowerCalculator/UIApp/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UIApp
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		//Otvara se prozor za odabir csv fajla za prognoziranu potrosnju
+		private void browseExpetedPower_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		//Otvara se prozor za odabir csv fajla za ostvarenu potrosnju
+		private void browseActualPower_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		//Validacija i primenjivanje podataka
+		private void importButton_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+	}
+}
