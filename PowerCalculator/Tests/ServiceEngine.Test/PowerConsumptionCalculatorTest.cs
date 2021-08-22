836f34b8d9f9ee7969fb8afe2bde00ffa88dce7b
Nenad020 <nenadzelenovic@gmail.com>
2021-08-22 18:17:59 +0200

    Zavrsni deo

---
+++ PowerCalculator/Tests/ServiceEngine.Test/PowerConsumptionCalculatorTest.cs
@@ -0,0 +1,114 @@
+﻿using Common.Helper.Interfaces;
+using Common.Model;
+using DatabaseAccess.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace ServiceEngine.Test
+{
+	public class PowerConsumptionCalculatorTest
+	{
+		private ISqliteDataAccess sqliteDataAccess;
+		private IDataTypeParser dataTypeParser;
+		private IMathHelper mathHelper;
+
+		private PowerConsumptionCalculator powerConsumptionCalculator;
+
+		[SetUp]
+		public void SetUp()
+		{
+			sqliteDataAccess = Substitute.For<ISqliteDataAccess>();
+			dataTypeParser = Substitute.For<IDataTypeParser>();
+			mathHelper = Substitute.For<IMathHelper>();
+
+			powerConsumptionCalculator = new PowerConsumptionCalculator(sqliteDataAccess, dataTypeParser, mathHelper);
+		}
+
+		[Test]
+		public void CalculatePower_CallsTheMethod_ReturnsOutputModel()
+		{
+			OutputModel actual = powerConsumptionCalculator.CalculatePower(new DateTime(2021, 5, 5), new DateTime(2021, 5, 10),
+				"VOJ");
+		}
+
+		[Test]
+		public void CheckRegionRange_CallsTheMethod_ReturnsList()
+		{
+			List<PowerRecord> powerRecords = new List<PowerRecord>()
+			{
+				new PowerRecord(1, "6-5-2021", 4000, "VOJ", "22-8-2021")
+			};
+
+			List<PowerRecord> actual = powerConsumptionCalculator.CheckRegionRange(new DateTime(1, 1, 1), new DateTime(1, 1, 1),
+				"VOJ", powerRecords);
+
+			Assert.AreEqual(actual.Count, 1);
+		}
+
+		[Test]
+		public void CalculateAbsoluteValues_CallsTheMethod_ReturnsList()
+		{
+			List<PowerRecord> expeted = new List<PowerRecord>()
+			{
+				new PowerRecord(1, "6-5-2021", 4000, "VOJ", "22-8-2021")
+			};
+
+			List<PowerRecord> actual = new List<PowerRecord>()
+			{
+				new PowerRecord(1, "6-5-2021", 4500, "VOJ", "22-8-2021")
+			};
+
+			List<CalculatedPower> output = powerConsumptionCalculator.CalculateAbsoluteValues(expeted, actual);
+
+			Assert.AreEqual(output.Count, 1);
+		}
+
+		[Test]
+		public void CalculateAbsoluteMeanDeviation_CallsTheMethod_ReturnsVoid()
+		{
+			List<CalculatedPower> absoluteValues = new List<CalculatedPower>()
+			{
+				new CalculatedPower(new DateTime(2021, 5, 6), new List<ConsumptionRecord>()
+				{
+					new ConsumptionRecord(1, new DateTime(2021, 5, 6), 50.55m, 50.55m),
+					new ConsumptionRecord(2, new DateTime(2021, 5, 6), 100, 100)
+				})
+			};
+
+			decimal output = powerConsumptionCalculator.CalculateAbsoluteMeanDeviation(absoluteValues);
+
+			mathHelper.
+				Received().
+				CalculateDivisionValue(150.55m, 2);
+		}
+
+		[Test]
+		public void CalculateSquareDeviation_CallsTheMethod_ReturnsVoid()
+		{
+			List<CalculatedPower> absoluteValues = new List<CalculatedPower>()
+			{
+				new CalculatedPower(new DateTime(2021, 5, 6), new List<ConsumptionRecord>()
+				{
+					new ConsumptionRecord(1, new DateTime(2021, 5, 6), 50.55m, 50.55m),
+					new ConsumptionRecord(2, new DateTime(2021, 5, 6), 100, 100)
+				})
+			};
+
+			double output = powerConsumptionCalculator.CalculateSquareDeviation(absoluteValues);
+
+			mathHelper.
+				Received().
+				CalculateDivisionValue(150.55m, 2);
+
+			mathHelper.
+				Received().
+				CalculateSquareValue(0);
+
+			mathHelper.
+				Received().
+				CalculateRootOfValue(0);
+		}
+	}
+}
---
+++ PowerCalculator/Tests/ServiceEngine.Test/PowerImporterTest.cs
@@ -0,0 +1,179 @@
+﻿using Common.FileUpload.Interfaces;
+using Common.Model;
+using DatabaseAccess.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace ServiceEngine.Test
+{
+	public class PowerImporterTest
+	{
+		private IFileDialog fileDialog;
+		private ISqliteDataAccess sqliteDataAccess;
+
+		private PowerImporter powerImporter;
+
+		[SetUp]
+		public void SetUp()
+		{
+			sqliteDataAccess = Substitute.For<ISqliteDataAccess>();
+			fileDialog = Substitute.For<IFileDialog>();
+
+			powerImporter = new PowerImporter(fileDialog, sqliteDataAccess);
+		}
+
+		[Test]
+		public void CollectData_CallsTheMethod_ReturnsVoid()
+		{
+			string expetedPath = @"D:\folder";
+			string actualPath = @"D:\folder2";
+
+			powerImporter.CollectData(expetedPath, actualPath);
+
+			fileDialog.
+				Received().
+				Load(expetedPath);
+
+			fileDialog.
+				Received().
+				Load(actualPath);
+		}
+
+		[Test]
+		public void ValidatePowerConsumtionTimePeriods_CallsTheMethod_ReturnsVoid()
+		{
+			powerImporter.ValidatePowerConsumtionTimePeriods(new List<PowerRecord>
+			{
+				new PowerRecord(1, "6-5-2021", 4500, "BG", "22-8-2021"),
+				new PowerRecord(2, "6-5-2021", 4000, "BG", "22-8-2021"),
+				new PowerRecord(3, "6-5-2021", 4000, "BG", "22-8-2021"),
+				new PowerRecord(4, "6-5-2021", 4000, "BG", "22-8-2021"),
+				new PowerRecord(3, "6-5-2021", 4000, "BG", "22-8-2021"),
+				new PowerRecord(3, "6-5-2021", 4000, "BG", "22-8-2021"),
+				new PowerRecord(3, "6-5-2021", 4000, "BG", "22-8-2021"),
+				new PowerRecord(3, "6-5-2021", 4000, "BG", "22-8-2021"),
+				new PowerRecord(3, "6-5-2021", 4000, "BG", "22-8-2021"),
+				new PowerRecord(3, "6-5-2021", 4000, "BG", "22-8-2021"),
+				new PowerRecord(3, "6-5-2021", 4000, "BG", "22-8-2021"),
+				new PowerRecord(3, "6-5-2021", 4000, "BG", "22-8-2021"),
+				new PowerRecord(3, "6-5-2021", 4000, "BG", "22-8-2021"),
+				new PowerRecord(3, "6-5-2021", 4000, "BG", "22-8-2021"),
+				new PowerRecord(3, "6-5-2021", 4000, "BG", "22-8-2021"),
+				new PowerRecord(3, "6-5-2021", 4000, "BG", "22-8-2021"),
+				new PowerRecord(3, "6-5-2021", 4000, "BG", "22-8-2021"),
+				new PowerRecord(3, "6-5-2021", 4000, "BG", "22-8-2021"),
+				new PowerRecord(3, "6-5-2021", 4000, "BG", "22-8-2021"),
+				new PowerRecord(3, "6-5-2021", 4000, "BG", "22-8-2021"),
+				new PowerRecord(3, "6-5-2021", 4000, "BG", "22-8-2021"),
+				new PowerRecord(3, "6-5-2021", 4000, "BG", "22-8-2021"),
+				new PowerRecord(3, "6-5-2021", 4000, "BG", "22-8-2021"),
+				new PowerRecord(3, "6-5-2021", 4000, "BG", "22-8-2021"),
+				new PowerRecord(3, "6-5-2021", 4000, "CRG", "22-8-2021")
+			});
+		}
+
+		[Test]
+		[TestCase(1)]
+		[TestCase(0)]
+		[TestCase(-10)]
+		[TestCase(20)]
+		[TestCase(30)]
+		[TestCase(null)]
+		public void CheckTimePeriodsNumber_ValidateInputParameters_ReturnsException(int numberOfHoursByRegion)
+		{
+			Exception ex = Assert.Throws<Exception>(() => powerImporter.CheckTimePeriodsNumber(numberOfHoursByRegion));
+
+			Assert.That(ex.Message == "Import files must have 23, 24 or 25 hours per region!");
+		}
+
+		[Test]
+		[TestCase(23)]
+		[TestCase(24)]
+		[TestCase(25)]
+		public void CheckTimePeriodsNumber_ValidateInputParameters_ReturnsVoid(int numberOfHoursByRegion)
+		{
+			powerImporter.CheckTimePeriodsNumber(numberOfHoursByRegion);
+		}
+
+		[Test]
+		public void ValidateFilesCount_ValidateInputParameters_ReturnsException()
+		{
+			List<PowerRecord> expeted = new List<PowerRecord>()
+			{
+				new PowerRecord(),
+				new PowerRecord()
+			};
+
+			List<PowerRecord> actual = new List<PowerRecord>()
+			{
+				new PowerRecord(),
+				new PowerRecord(),
+				new PowerRecord()
+			};
+
+			Exception ex = Assert.Throws<Exception>(() => powerImporter.ValidateFilesCount(expeted, actual));
+
+			Assert.That(ex.Message == "Import files don't have equal data rows, please import valid files!");
+		}
+
+		[Test]
+		public void ValidateFilesCount_ValidateInputParameters_ReturnsVoid()
+		{
+			List<PowerRecord> expeted = new List<PowerRecord>()
+			{
+				new PowerRecord(),
+				new PowerRecord(),
+				new PowerRecord()
+			};
+
+			List<PowerRecord> actual = new List<PowerRecord>()
+			{
+				new PowerRecord(),
+				new PowerRecord(),
+				new PowerRecord()
+			};
+
+			powerImporter.ValidateFilesCount(expeted, actual);
+		}
+
+		[Test]
+		public void ValidateDataFromFiles_ValidateInputParameters_ReturnsException()
+		{
+			List<PowerRecord> expeted = new List<PowerRecord>()
+			{
+				new PowerRecord(1, "6-5-2021", 4500, "BG", "22-8-2021"),
+				new PowerRecord(2, "6-5-2021", 4000, "VOJ", "22-8-2021")
+			};
+
+			List<PowerRecord> actual = new List<PowerRecord>()
+			{
+				new PowerRecord(2, "6-5-2021", 4500, "VOJ", "22-8-2021"),
+				new PowerRecord(3, "6-5-2021", 4000, "VOJ", "22-8-2021")
+			};
+
+			Exception ex = Assert.Throws<Exception>(() => powerImporter.ValidateDataFromFiles(expeted, actual));
+
+			Assert.That(ex.Message == "Files are inconsistent, please import valid files!");
+		}
+
+		[Test]
+		public void ValidateDataFromFiles_ValidateInputParameters_ReturnsVoid()
+		{
+			List<PowerRecord> expeted = new List<PowerRecord>()
+			{
+				new PowerRecord(1, "6-5-2021", 4500, "VOJ", "22-8-2021"),
+				new PowerRecord(2, "6-5-2021", 4000, "VOJ", "22-8-2021")
+			};
+
+			List<PowerRecord> actual = new List<PowerRecord>()
+			{
+				new PowerRecord(1, "6-5-2021", 4500, "VOJ", "22-8-2021"),
+				new PowerRecord(2, "6-5-2021", 4000, "VOJ", "22-8-2021")
+			};
+
+			powerImporter.ValidateDataFromFiles(expeted, actual);
+		}
+	}
+}
